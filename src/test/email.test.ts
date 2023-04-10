150e3c7cc6f568008e90c8593c826de77d7f8a53
Fernando Perera <fernando02perera@gmail.com>
2023-04-10 12:53:38 +0200

    Test(red): email have a wrong format return error response test failed, not implemented yet.

--- src/test/email.test.ts
+++ src/test/email.test.ts
@@ -1,8 +1,9 @@
 import { Email } from '../core/models/Email';
 
 /**
- *  - if email was empty    ->    error response
- *  - if email was correct  ->    return same email
+ *  - if email was empty                ->    error response
+ *  - if email have a wrong format      ->    error response
+ *  - if email was correct              ->    return same email
  */
 
 describe('email test', () => {
@@ -10,6 +11,12 @@ describe('email test', () => {
     it('email was empty return error response', () => {
 
         expect( () => Email.createNewEmail('') ).toThrow('Email couldnt be empty')
+
+    })
+
+    it('email have a wrong format return error response', () => {
+
+        expect( () => Email.createNewEmail('') ).toThrow('Email have a wrong format')
 
     })
 
