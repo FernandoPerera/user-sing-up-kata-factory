5f1fdf179bd58a5fd70e0197c285d0e2bad5158e
Fernando Perera <fernando02perera@gmail.com>
2023-04-10 11:35:37 +0200

    if name have a correct content return the same string test, not implemented yet.

--- src/test/name.test.ts
+++ src/test/name.test.ts
@@ -1,14 +1,23 @@
 import { Name } from '../core/models/Name';
 
 /*
- *  - if name was empty  ->  error response
+ *  - if name was empty             ->  error response
+ *  - if name have correct content  ->  return same string
 */
 
 describe('name test', () => {
 
-    it('name was empty return error response', () => {
+    it('if name was empty return error response', () => {
 
         expect( () => Name.createNewName('') ).toThrow('Name cant be a empty string')
+
+    })
+
+    it('if name have a correct content return the same string', () => {
+
+        const name: Name = Name.createNewName('Fernando')
+
+        expect(name).toBe('Fernando')
 
     })
 
