84cc2e55e1283ab248f952ee458a328d0877b2cd
Fernando Perera <fernando02perera@gmail.com>
2023-04-10 11:23:45 +0200

    Test(green): name was empty return error response test passed, adding a factory method with a conditional.

--- src/core/models/Name.ts
+++ src/core/models/Name.ts
@@ -3,8 +3,18 @@ export class Name {
 
     private name: string
 
-    constructor( name: string ){
+    private constructor( name: string ){
         this.name = name
+    }
+
+    static createNewName( name: string ): Name {
+
+        if( name !== '' ) {
+            return new Name(name)
+        }
+
+        throw new Error('Name cant be a empty string')
+        
     }
 
 }
--- src/test/name.test.ts
+++ src/test/name.test.ts
@@ -1,16 +1,14 @@
 import { Name } from '../core/models/Name';
 
 /*
- *  - if name, email or age was empty  ->  error response
+ *  - if name was empty  ->  error response
 */
 
 describe('name test', () => {
 
     it('name was empty return error response', () => {
 
-        const name: Name = new Name('')
-
-        expect(name).toThrow('Name cant be a empty string')
+        expect( () => Name.createNewName('') ).toThrow('Name cant be a empty string')
 
     })
 
