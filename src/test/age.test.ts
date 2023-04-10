1803199da609f46509bc20d48ed1d2cf705a771e
Fernando Perera <fernando02perera@gmail.com>
2023-04-10 12:32:39 +0200

    Test(red): if age was a correct number return same age test failed, not implemented yet.

--- src/test/age.test.ts
+++ src/test/age.test.ts
@@ -3,7 +3,7 @@ import { Age } from '../core/models/Age';
 /**
  *  - if age was equals or less than zero      ->   error response
  *  - if age was a float                       ->   error response
- *  - if age was a negative number             ->   error response
+ *  - if age was correct                       ->   return the same age   
  */
 
 describe('age test', () => {
@@ -17,6 +17,14 @@ describe('age test', () => {
     it('if age was a float return error response', () => {
 
         expect( () => Age.createNewAge(17.89) ).toThrow('Age have to be a whole number')
+
+    })
+
+    it('if age was a correct number return same age', () => {
+
+        const age: Age = Age.createNewAge(17)
+
+        expect( age ).toBe(17)
 
     })
 
