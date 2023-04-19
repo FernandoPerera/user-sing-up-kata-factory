4ee90a0d95b8c86f7ab8f750adb6347eb98f9089
Fernando Perera <fernando02perera@gmail.com>
2023-04-19 09:25:26 +0200

    Test(fixed): Adding myFakeUserRepository for instance the UserRepository and be able to delete the FakeUserRepository class.

--- src/test/saveData.test.ts
+++ src/test/saveData.test.ts
@@ -1,5 +1,4 @@
 
-import { FakeUserRepository } from '../core/repository/FakeUserRepository'
 import { UserService } from '../core/service/UserService'
 import { User } from '../core/models/User'
 import { Email } from '../core/models/Email'
@@ -13,35 +12,23 @@ import { UserRepository } from '../core/repository/UserRepository';
 */
 
 describe('sing-up user test', () => {
-    let fakeUserRepository: FakeUserRepository
-    let userService: UserService
-
-    let saveSpy: jest.SpyInstance<void, [user: User], any>
-    let userExistSpy: jest.SpyInstance<boolean, [user: User], any>
     
-    let name: Name
-    let age: Age
-    let email: Email
-    let user: User
-
-    beforeEach( () => {
-
-        fakeUserRepository = new FakeUserRepository()
-        userService = new UserService(fakeUserRepository)
-
-        saveSpy = jest.spyOn(fakeUserRepository, 'save')
-        userExistSpy = jest.spyOn(fakeUserRepository, 'userExist')
-
-        name = Name.createNewName('Fernando')
-        age = Age.createNewAge(20)
-        email = Email.createNewEmail('example@gmail.com')
-        user = new User(name, age, email)
-        
-    })
+    const name: Name = Name.createNewName('Fernando')
+    const age: Age = Age.createNewAge(20)
+    const email: Email = Email.createNewEmail('example@gmail.com')
+    const user: User = new User(name, age, email)
 
     it('if user is correct insert into the database', () => {
 
-        userService.save(user)
+        const myFakeUserRepository: UserRepository = {
+            save: jest.fn(),
+            userExist: () => false
+        }
+
+        const myUserService = new UserService(myFakeUserRepository)
+        const saveSpy = jest.spyOn(myFakeUserRepository, 'save')
+
+        myUserService.save(user)
 
         expect(saveSpy).toHaveBeenCalledWith(user)
 
@@ -53,6 +40,7 @@ describe('sing-up user test', () => {
             save: jest.fn(),
             userExist: () => true
         }
+        
         const myUserService = new UserService(myFakeUserRepository)
 
         myUserService.save(user)
